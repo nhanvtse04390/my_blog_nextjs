90d30538acbc098dccfcbb722c14ee1718773f64
votrongnhan <nhanvt@mor.com.vn>
2025-03-02 23:46:52 +0700

    check CORS

--- src/app/services/api.js
+++ src/app/services/api.js
@@ -1,7 +1,7 @@
 import axiosClient from '../utils/axiosConfig';
 
 export const fetchData = async () => {
-  const response = await axiosClient.get('express-production-0dd6.up.railway.app:8080/api/users');
+  const response = await axiosClient.get('https://express-production-0dd6.up.railway.app/api/users');
   console.log("vao day chua",response)
 
   return response;
