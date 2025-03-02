df008446ffe644d1ba6ad526d49df8e527a71f15
votrongnhan <nhanvt@mor.com.vn>
2025-03-02 23:15:54 +0700

    deploy

--- src/app/services/api.js
+++ src/app/services/api.js
@@ -1,8 +1,8 @@
 import axiosClient from '../utils/axiosConfig';
 
 export const fetchData = async () => {
-  const response = await axiosClient.get('http://localhost:8080/api/users');
+  const response = await axiosClient.get('express-production-0dd6.up.railway.app:8080/api/users');
   console.log("vao day chua",response)
 
   return response;
-};
+};  
