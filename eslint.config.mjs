a7befc94bf93680d9b73e6678de3c84863bf22a8
votrongnhan <nhanvt@mor.com.vn>
2025-03-09 21:38:08 +0700

    a

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -9,13 +9,12 @@ const compat = new FlatCompat({
     baseDirectory: __dirname,
 });
 
-const eslintConfig = {
+export default [
     ...compat.extends("next/core-web-vitals", "next/typescript"),
-    extends: ["next"],
-    rules: {
-        "react/no-unescaped-entities": "off",
-        "@next/next/no-page-custom-font": "off",
+    {
+        rules: {
+            "react/no-unescaped-entities": "off",
+            "@next/next/no-page-custom-font": "off",
+        },
     },
-};
-
-export default eslintConfig;
+];
