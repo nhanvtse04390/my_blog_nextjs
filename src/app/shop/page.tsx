2fa0383126da1c160ed981006037b0787e1d7503
nhanvt <nhanvt@mor.com.vn>
2025-03-19 14:08:41 +0700

    real  data

--- src/app/shop/page.tsx
+++ src/app/shop/page.tsx
@@ -1,42 +1,32 @@
 "use client"
 import FeaturedCarousel from "@/app/layout/FeaturedCarousel";
 import ProductCard from "../components/ProductCard";
-import {useEffect, useState} from "react";
+import React, {useEffect, useState} from "react";
 import {Product} from "@/app/types/product";
 import {getProduct} from "@/app/api/product";
 import {AxiosError} from "axios";
 import {PARAMS} from "@/app/admin/product/list/page";
 import {useError} from "@/app/components/ErrorProvider";
+import {Pagination} from "@mui/material";
 
-
-// const products = [
-//   {id: 0, name: "Áo thun nam", price: 250000, image: "/images/shirt.jpg", description: "sản phẩm đang được hỗ trợ trả góp sản phẩm đang được hỗ trợ trả góp phẩm đang được hỗ trợ trả góp phẩm đang được hỗ trợ trả góp phẩm đang được hỗ trợ trả góp phẩm đang được hỗ trợ trả góp phẩm đang được hỗ trợ trả góp phẩm đang được hỗ trợ trả góp phẩm đang được hỗ trợ trả góp phẩm đang được hỗ trợ trả góp", discount: 10},
-//   {id: 1, name: "Áo thun nam", price: 250000, image: "/images/shirt.jpg", description: "sản phẩm đang được hỗ trợ trả góp"},
-//   {id: 2, name: "Giày sneaker", price: 1200000, image: "/images/shoes.jpg", description: "sản phẩm đang được hỗ trợ trả góp"},
-//   {id: 3, name: "Balo thời trang", price: 550000, image: "/images/backpack.jpg", description: "sản phẩm đang được hỗ trợ trả góp"},
-//   {id: 4, name: "Tai nghe Bluetooth", price: 890000, image: "/images/headphones.jpg", description: "sản phẩm đang được hỗ trợ trả góp"},
-//   {id: 5, name: "Tai nghe Bluetooth", price: 890000, image: "/images/headphones.jpg", description: "sản phẩm đang được hỗ trợ trả góp"},
-//   {id: 6, name: "Tai nghe Bluetooth", price: 890000, image: "/images/headphones.jpg", description: "sản phẩm đang được hỗ trợ trả góp"},
-//   {id: 7, name: "Tai nghe Bluetooth", price: 890000, image: "/images/headphones.jpg", description: "sản phẩm đang được hỗ trợ trả góp"},
-//   {id: 8, name: "Tai nghe Bluetooth", price: 890000, image: "/images/headphones.jpg", description: "sản phẩm đang được hỗ trợ trả góp"},
-//   {id: 9, name: "Tai nghe Bluetooth", price: 890000, image: "/images/headphones.jpg", description: "sản phẩm đang được hỗ trợ trả góp"},
-//   {id: 10, name: "Tai nghe Bluetooth", price: 890000, image: "/images/headphones.jpg", description: "sản phẩm đang được hỗ trợ trả góp"},
-//   {id: 11, name: "Tai nghe Bluetooth", price: 890000, image: "/images/headphones.jpg", description: "sản phẩm đang được hỗ trợ trả góp"},
-//   {id: 12, name: "Tai nghe Bluetooth", price: 890000, image: "/images/headphones.jpg", description: "sản phẩm đang được hỗ trợ trả góp"},
-//   {id: 13, name: "Tai nghe Bluetooth", price: 890000, image: "/images/headphones.jpg", description: "sản phẩm đang được hỗ trợ trả góp"},
-//   {id: 14, name: "Tai nghe Bluetooth", price: 890000, image: "/images/headphones.jpg", description: "sản phẩm đang được hỗ trợ trả góp"},
-//   {id: 15, name: "Tai nghe Bluetooth", price: 890000, image: "/images/headphones.jpg", description: "sản phẩm đang được hỗ trợ trả góp"},
-// ];
+export type PARAMS = {
+  page: number;
+  rowsPerPage: number;
+};
 
 export default function Shop() {
   const {showError} = useError();
   const [products, setProducts] = useState<Product[]>([])
+  const [page, setPage] = React.useState(0);
+  const [rowsPerPage] = React.useState(10);
+  const [totalItems, setTotalPage] = useState<number>(0)
   useEffect(() => {
     const fetchProducts = async () => {
       try {
-        const params: PARAMS = {page: 1, rowsPerPage: 20};
+        const params: PARAMS = {page, rowsPerPage};
         const response = await getProduct(params);
         setProducts(response.data.list)
+        setTotalPage(response.data.pagination.totalItems)
       } catch (error) {
         const err = error as AxiosError;
         showError(err.message)
@@ -44,7 +34,7 @@ export default function Shop() {
     };
 
     fetchProducts();
-  }, []);
+  }, [page]);
 
   return (
     <div className="max-w-7xl mx-auto p-2">
@@ -59,6 +49,28 @@ export default function Shop() {
           <ProductCard key={product._id} product={product}/>
         ))}
       </div>
+      <Pagination
+        count={Math.ceil(totalItems / rowsPerPage)}
+        page={page}
+        onChange={(_, value) => setPage(value)}
+        sx={{
+          display: "flex",
+          justifyContent: "center",
+          marginTop: 2,
+          "& .MuiPaginationItem-root": {
+            color: "#fff",
+            backgroundColor: "#1976d2",
+            borderRadius: "8px",
+            "&:hover": {
+              backgroundColor: "#1565c0",
+            },
+            "&.Mui-selected": {
+              backgroundColor: "#004ba0",
+              color: "#fff",
+            },
+          },
+        }}
+      />
     </div>
   );
 }
