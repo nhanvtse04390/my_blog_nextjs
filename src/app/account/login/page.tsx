43b0793512942fdd229c7eb7da40d92e26875051
nhanvt <nhanvt@mor.com.vn>
2025-03-26 10:56:14 +0700

    register with code ref

--- src/app/account/login/page.tsx
+++ src/app/account/login/page.tsx
@@ -70,7 +70,7 @@ export default function Login() {
                             <input
                                 type="password"
                                 name="password"
-                                className="w-full px-4 py-2 rounded-xl focus:outline-none focus:ring-2 focus:ring-blue-500 bg-black"
+                                className="w-full px-4 py-2 rounded-xl focus:outline-none focus:ring-2 focus:ring-blue-500 bg-black text-white"
                                 placeholder="Nhập mật khẩu"
                                 value={formData.password}
                                 onChange={handleChange}
