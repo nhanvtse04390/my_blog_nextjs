981d40b883c6561eff6af0243ea0713c8032d187
nhanvt <nhanvt@mor.com.vn>
2025-03-28 10:23:31 +0700

    checkout

--- src/app/shop/checkout/page.tsx
+++ src/app/shop/checkout/page.tsx
@@ -83,7 +83,7 @@ export default function CheckoutPage() {
 
       const param = { ...data, totalAmount, userId: info._id, items: cart };
       const response = await createOrder(param);
-      showSuccess(response.data.message);
+      response && response.data.message ? showSuccess(response.data.message) : "";
       clearCart();
       router.push("/shop");
     } catch (error) {
